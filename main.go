71d4ffe28005b30b451d3e323d5fd32e9c96bcdb
Rodrigo Oliveira <rodrigooliveirareis98@gmail.com>
2026-05-11 20:10:16 -0300

    feat: implement echo and type shell builtins

--- main.go
+++ main.go
@@ -7,30 +7,39 @@ import (
 	"strings"
 )
 
-
 func main() {
-	
+
 	reader := bufio.NewReader(os.Stdin)
-
-	var builtins map[string]func([]string)
-
+	builtins := map[string]func([]string){}
 	builtins = map[string]func([]string){
 
-		"echo": func(s []string) {		},
-		"exit":func(s []string) {
+		"echo": func(args []string) {
+			fmt.Println(strings.Join(args, " "))
+		},
+		"exit": func(args []string) {
 			os.Exit(0)
 		},
-		"type":func(s []string) {},
-
+		"type": func(args []string) {
+			
+			if len(args)==0 {
+				return
+			}
+			command := args[0]
+			if _,ok :=builtins[command];ok{
+				fmt.Printf("%s is a shell builtin\n",args[0])
+			}else{
+				fmt.Printf("%s: not found\n", command)
+			}
+		},
 	}
 
-	for{
+	for {
 		fmt.Print("$ ")
 
-		input,err := reader.ReadString('\n')
+		input, err := reader.ReadString('\n')
 
-		if err != nil{
-			fmt.Print("Error input ",err)
+		if err != nil {
+			fmt.Print("Error input ", err)
 		}
 
 		prompt := strings.TrimSpace(input)
@@ -38,20 +47,17 @@ func main() {
 		if prompt == "" {
 			continue
 		}
-		
-		parts :=strings.Fields(prompt)
-		cmd := parts[0]
-		args :=parts[1:]
 
-		if fn,ok := builtins[cmd];ok{
+		parts := strings.Fields(prompt)
+		cmd := parts[0]
+		args := parts[1:]
+
+		if fn, ok := builtins[cmd]; ok {
 			fn(args)
+		} else {
+			fmt.Println(cmd, ":Not found")
 		}
 
-
 	}
-
-
-
-
 
 }
