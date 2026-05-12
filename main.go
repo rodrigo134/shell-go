2f6e444c6960bd5ebebd20c0b2b4dfb07e2bec4d
Rodrigo Oliveira <rodrigooliveirareis98@gmail.com>
2026-05-11 21:46:42 -0300

    feat: implement pwd builtin and external command execution from PATH

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -16,9 +17,9 @@ func main() {
 		"echo": func(args []string) {
 			fmt.Println(strings.Join(args, " "))
 		},
-		"pwd":func(args []string) {
-			dir,err := os.Getwd()
-			if err != nil{
+		"pwd": func(args []string) {
+			dir, err := os.Getwd()
+			if err != nil {
 				fmt.Println("Error getting current directory")
 				return
 			}
@@ -29,16 +30,17 @@ func main() {
 			os.Exit(0)
 		},
 		"type": func(args []string) {
-			
-			if len(args)==0 {
+
+			if len(args) == 0 {
 				return
 			}
 			command := args[0]
-			if _,ok :=builtins[command];ok{
-				fmt.Printf("%s is a shell builtin\n",args[0])
-			}else{
+			if _, ok := builtins[command]; ok {
+				fmt.Printf("%s is a shell builtin\n", args[0])
+			} else {
 				fmt.Printf("%s: not found\n", command)
 			}
+
 		},
 	}
 
@@ -58,15 +60,24 @@ func main() {
 		}
 
 		parts := strings.Fields(prompt)
-		cmd := parts[0]
+		commands := parts[0]
 		args := parts[1:]
 
-		if fn, ok := builtins[cmd]; ok {
+		if fn, ok := builtins[commands]; ok {
 			fn(args)
 		} else {
-			fmt.Println(cmd, ":Not found")
+			path, err := exec.LookPath(commands) //path = /usr/bin/ls
+			if err != nil {
+				fmt.Println(commands + ": not found")
+				continue
+
+			}
+			cmd := exec.Command(path, args...) ///usr/bin/ls -la
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Run()
+
 		}
-
 	}
-
 }
